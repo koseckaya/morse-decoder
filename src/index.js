27952fe6aa5fbea1d776afcc92e41eb3265432a8
interika <interika11@gmail.com>
2022-07-13 15:21:45 +0300

    'ready'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    let resultString = '';
+
+    for (let i=0; i< expr.length; i += 10) {
+       let morzeLetter = '';
+        for(let j=i + 9; j > i; j -= 2 ) {
+            let block = expr[j-1] + expr[j];
+            if (block === '**') {
+                resultString += ' ';
+                break;
+            }
+      
+            switch (block) {
+                case '00':
+                    break;
+                case '10':
+                    morzeLetter = '.' + morzeLetter
+                    break;
+                case '11':
+                    morzeLetter = '-' + morzeLetter
+                    break;
+            }
+        }
+  
+        if (morzeLetter.length) {
+            resultString += MORSE_TABLE[morzeLetter];
+        }
+    }
+
+return resultString;
 }
 
 module.exports = {
